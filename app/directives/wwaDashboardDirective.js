1b3f4f3aa6a660d90e7c004c1dda3ed3d6cb2830
Bryan Clover <bclover09@gmail.com>
2015-08-17 13:56:58 -0400

    Added modals to allow user to change the widget settings or close it. Added additional employee photos.

--- app/directives/wwaDashboardDirective.js
+++ app/directives/wwaDashboardDirective.js
@@ -29,7 +29,9 @@ angular.module('app').directive('wwaDashboard',
 								minSizeY: 4,
 								template: '<wwa-temperature></wwa-temperature>',
 								widgetSettings: {
-									id: 1000
+									id: 1000,
+									templateUrl: 'app/dialogs/wwwSelectLocationTemplate.html',
+									controller: 'wwaSelectLocationController'
 								}
 							}
 						},
@@ -42,7 +44,9 @@ angular.module('app').directive('wwaDashboard',
 								minSizeY: 4,
 								template: '<wwa-inventory></wwa-inventory>',
 								widgetSettings: {
-									id: 1002
+									id: 1002,
+									templateUrl: 'app/dialogs/wwwSelectLocationTemplate.html',
+									controller: 'wwaSelectLocationController'
 								}
 							}
 						},
@@ -55,52 +59,16 @@ angular.module('app').directive('wwaDashboard',
 								minSizeY: 4,
 								template: '<wwa-employee></wwa-employee>',
 								widgetSettings: {
-									id: 5001
+									id: 5001,
+									templateUrl: 'app/dialogs/wwwSelectEmployeeTemplate.html',
+									controller: 'wwaSelectEmployeeController'
 								}
 							}
 						}
 					];
 
 					scope.widgets = [
-						{
-							title: 'First',
-							sizeX: 4,
-							sizeY: 4,
-							minSizeX: 4,
-							minSizeY: 4,
-							row: 0,
-							col: 0,
-							template: '<wwa-temperature></wwa-temperature>',
-							widgetSettings: {
-								id: 1000
-							}
-						},
-						{
-							title: 'Second',
-							sizeX: 5,
-							sizeY: 4,
-							minSizeX: 5,
-							minSizeY: 4,
-							row: 0,
-							col: 5,
-							template: '<wwa-employee></wwa-employee>',
-							widgetSettings: {
-								id: 5001
-							}
-						},
-						{
-							title: 'Third',
-							sizeX: 6,
-							sizeY: 4,
-							minSizeX: 6,
-							minSizeY: 4,
-							row: 5,
-							col: 5,
-							template: '<wwa-inventory></wwa-inventory>',
-							widgetSettings: {
-								id: 1002
-							}
-						}
+
 					];
 				}
 			};
--- ext-modules/psDashboard/psWidgetBodyDirective.js
+++ ext-modules/psDashboard/psWidgetBodyDirective.js
@@ -1,14 +1,34 @@
 'use strict';
 
 angular.module('psDashboard').directive('psWidgetBody',
-	['$compile',
-		function($compile){
+	['$compile', '$modal',
+		function($compile, $modal){
 			return {
 				templateUrl: 'ext-modules/psDashboard/psWidgetBodyTemplate.html',
 				link: function(scope, element, attrs) {
 					var newElement = angular.element(scope.item.template);
 					element.append(newElement);
 					$compile(newElement)(scope);
+
+					scope.close = function() {
+						scope.widgets.splice(scope.widgets.indexOf(scope.item), 1);
+					};
+
+					scope.settings = function() {
+						var options = {
+							templateUrl: scope.item.widgetSettings.templateUrl,
+							controller: scope.item.widgetSettings.controller,
+							scope: scope
+						};
+						$modal.open(options);
+					};
+
+					scope.iconClicked = function() {
+						//This empty function body is on purpose!
+						//It allows angular to handle events for icons that are tapped
+						//when the user is on a mobile device. It prevents the widget from
+						//intercepting the events. Without this...tapping icons would not work.
+					};
 				}
 			};
 		}
